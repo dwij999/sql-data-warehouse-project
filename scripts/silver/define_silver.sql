69ebf919b687b406c4af3f55fbbd5afbfebc5dd6
dwij999 <dwijchaubey@gmail.com>
2026-03-03 11:16:50 +0530

    Update define_silver.sql

--- scripts/silver/define_silver.sql
+++ scripts/silver/define_silver.sql
@@ -1,1 +1,105 @@
 
+-- This script defines the Silver Layer tables for the data warehouse project. It ensures clean recreation by dropping existing tables before creating new ones. The schema includes:
+
+-- CRM Tables
+
+-- crm_cust_info: Customer details (ID, key, name, marital status, gender, creation date).
+
+-- crm_prd_info: Product details (ID, key, name, cost, product line, start/end dates).
+
+-- crm_sales_details: Sales transactions (order number, product key, customer ID, dates, sales amount, quantity, price).
+
+-- ERP Tables
+
+-- erp_loc_a101: Customer location mapping (customer ID, country).
+
+-- erp_cust_az12: Customer demographics (customer ID, birthdate, gender).
+
+-- erp_px_cat_g1v2: Product categorization (ID, category, subcategory, maintenance info).
+
+
+
+
+
+
+
+
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_cust_info;
+GO
+
+CREATE TABLE silver.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
+GO
+
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;
+GO
+
+CREATE TABLE silver.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+GO
+
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
+GO
+
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
+
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101;
+GO
+
+CREATE TABLE silver.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12;
+GO
+
+CREATE TABLE silver.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
